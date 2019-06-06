230ded57bacc2bec97a3a2408a37c576a750c0d4
PK-Mac <partheeban.kandasamy@gmail.com>
2019-06-05 20:49:39 -0700

    Flask + Mongo Docker + DockerCompose = Update items in DB

--- web/app.py
+++ web/app.py
@@ -1,25 +1,33 @@
-import time
-
-import redis
-from flask import Flask
+import os
+from flask import Flask, redirect, url_for, request, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
-
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+client = MongoClient(
+    os.environ['DB_PORT_27017_TCP_ADDR'],27017)
+db = client.tododb
 
 
 @app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Hello from Docker! I have been seen {} times.\n'.format(count)
+def todo():
+
+    _items = db.tododb.find()
+    items = [item for item in _items]
+
+    return render_template('todo.html', items=items)
+
+
+@app.route('/new', methods=['POST'])
+def new():
+
+    item_doc = {
+        'name': request.form['name'],
+        'description': request.form['description']
+    }
+    db.tododb.insert_one(item_doc)
+
+    return redirect(url_for('todo'))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
